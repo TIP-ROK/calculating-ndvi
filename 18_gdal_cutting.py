fe83bbc074addfbf2ba30faac6b87bbcfd702eaf
TIP-ROK <chenye797@gmail.com>
2023-03-27 18:12:29 +0600

    fixed bag cutting with gdal

--- 18_gdal_cutting.py
+++ 18_gdal_cutting.py
@@ -1,34 +1,21 @@
-from osgeo import gdal, osr
+from osgeo import gdal
+import os
 
 in_path = 'ds/20220428/tif/'
-input_filename = 'T43TCH_20220428T055629_B02_20m.tif'
+input_filename = 'T43TCH_20220428T055629_B04_20m.tif'
 
-out_path = 'ds/20220428/gdal_cut-1/'
-output_filename = 'tile_'
+out_path = 'ds/20220428/cutting_1/'
+output_filename = 'tile_B04_'
 
-tile_size_x = 256
-tile_size_y = 256
+tile_size_x = 640
+tile_size_y = 640
 
-src_ds = gdal.Open(in_path + input_filename)
-src_srs = osr.SpatialReference(wkt=src_ds.GetProjection())
-
-dst_srs = osr.SpatialReference()
-dst_srs.ImportFromEPSG(4326)
-
-transform = osr.CoordinateTransformation(src_srs, dst_srs)
-
-xsize = src_ds.RasterXSize
-ysize = src_ds.RasterYSize
+ds = gdal.Open(in_path + input_filename)
+band = ds.GetRasterBand(1)
+xsize = band.XSize
+ysize = band.YSize
 
 for i in range(0, xsize, tile_size_x):
     for j in range(0, ysize, tile_size_y):
-        xmin = i
-        ymin = j
-        xmax = i + tile_size_x
-        ymax = j + tile_size_y
-        if xmax > xsize:
-            xmax = xsize
-        if ymax > ysize:
-            ymax = ysize
-        dst_filename = out_path + output_filename + str(i) + "_" + str(j) + ".tif"
-        gdal.Warp(dst_filename, src_ds, format='GTiff', outputBounds=[xmin, ymin, xmax, ymax], xRes=0.0001, yRes=0.0001, dstSRS='EPSG:4326')
+        com_string = "gdal_translate -of GTIFF -srcwin " + str(i) + ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + str(i) + "_" + str(j) + ".tif"
+        os.system(com_string)
